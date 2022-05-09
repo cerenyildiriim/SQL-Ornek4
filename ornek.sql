f17f63df3ac25264832bf2765330d38f80efd942
Ceren Yıldırım <72028430+cerenyildiriim@users.noreply.github.com>
2022-05-09 13:30:32 +0300

    Create ornek.sql

---
+++ ornek.sql
@@ -0,0 +1,19 @@
+1)Film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
+
+SELECT DISTINCT replacement_cost FROM film;
+
+')Film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
+
+SELECT COUNT(DISTINCT replacement_cost) FROM film;
+
+3)Film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
+
+SELECT COUNT(*) FROM film WHERE title LIKE 'T%' AND rating = 'G';
+
+4)country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
+
+SELECT COUNT(country LIKE '_____') FROM country;
+
+5)city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
+
+SELECT COUNT(city ILIKE '_%r') FROM city;
